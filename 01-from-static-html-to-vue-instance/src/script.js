0f50e50ee82ffa80eeda06e65d91602301e34499
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-04 22:12:25 +0200

    feat: adds a custom directive

--- 01-from-static-html-to-vue-instance/src/script.js
+++ 01-from-static-html-to-vue-instance/src/script.js
@@ -4,4 +4,14 @@ Vue.createApp({
         userName: "Berkant Cetin",
       };
     },
+    directives: {
+      highlight: { // custom directive
+        mounted(el) {
+          el.style.backgroundColor = "#6a1cc3";
+          el.style.color = "white";
+          el.style.borderRadius = ".625rem";
+          el.style.padding = ".188rem";
+        }
+      }
+    }
   }).mount("#app");
