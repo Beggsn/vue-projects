fe90ae89b523bce5f7ff5e6f9fbdd131591bdb98
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-06 15:45:48 +0200

    feat: adds style binding & directives

--- apps-with-vue/counter/src/script.js
+++ apps-with-vue/counter/src/script.js
@@ -4,4 +4,13 @@ Vue.createApp({
       count: 0,
     };
   },
+  methods: {
+    handleWheel(event) {
+      if (event.deltaY < 0) {
+        this.count++; // Nach oben scrollen
+      } else if (event.deltaY > 0 && this.count > 0) {
+        this.count--; // nur wenn count größer als 0 ist
+      }
+    }       
+  }
 }).mount("#app");
