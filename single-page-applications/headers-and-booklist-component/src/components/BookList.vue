47f6a4ba950a6f722758d0b182fb8b21b43a01df
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-12 11:30:14 +0200

    feat: adds booklist component

---
+++ single-page-applications/headers-and-booklist-component/src/components/BookList.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="table-item">
+    <h2 class="table-item__hl">All Books</h2>
+    <table class="table-item__table">
+      <thead>
+        <tr>
+          <th class="table-item__table-head-name">Name</th>
+          <th class="table-item__table-head--isbn">ISBN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="book in books"
+          :key="book.isbn"
+          class="table-item__table-row"
+        >
+          <td>{{ book.title }}</td>
+          <td>{{ book.isbn }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      books: [
+        {
+          title: "Practical Rust Web Projects",
+          isbn: "9781484265888",
+          author: "Shing Lyu",
+          publisher: "Apress",
+          price: "$28.75",
+          numPages: 256,
+        },
+        {
+          title: "Using WebPagetest",
+          isbn: "9781491902592",
+          author: "Rick Viscomi, Andy Davies, Marcel Duran",
+          publisher: "O'Reilly Media",
+          price: "$25.80",
+          numPages: 214,
+        },
+        {
+          title: "Web Scraping with Python",
+          isbn: "9781491910290",
+          author: "Ryan Mitchell",
+          publisher: "O'Reilly Media",
+          price: "$14.00",
+          numPages: 256,
+        },
+        {
+          title: "High Performance Mobile Web",
+          isbn: "9781491912553",
+          author: "Maximiliano Firtman",
+          publisher: "O'Reilly Media",
+          price: "$7.00",
+          numPages: 326,
+        },
+      ],
+    };
+  },
+};
+</script>
