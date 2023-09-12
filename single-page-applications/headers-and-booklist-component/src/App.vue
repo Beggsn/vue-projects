5377717db61e55bb1871e0372fe1cacbaf13950c
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-12 11:20:36 +0200

    init: adds project files and basic styling

---
+++ single-page-applications/headers-and-booklist-component/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div id="app" class="inside">
+      <BookList />
+    </div>
+  </main>
+</template>
+
+<script>
+import BookList from "./components/BookList.vue";
+
+export default {
+  name: "App",
+  components: {
+    BookList,
+  },
+};
+</script>
