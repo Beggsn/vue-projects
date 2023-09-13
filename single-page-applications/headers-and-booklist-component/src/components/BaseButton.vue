8aaceda439ba3342b4328f7d81d5c7b807325ec4
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-09-13 11:48:50 +0200

    refac: dissolves global css and adds custom event

--- single-page-applications/headers-and-booklist-component/src/components/BaseButton.vue
+++ single-page-applications/headers-and-booklist-component/src/components/BaseButton.vue
@@ -1,9 +1,10 @@
 <template>
-  <button :class="styling">{{ text }}</button>
+  <button :class="styling" @click="$emit('btn-clicked')">{{ text }}</button>
 </template>
 
 <script>
 export default {
+  emits: ["btn-clicked"],
   props: {
     text: {
       type: String,
@@ -24,3 +25,35 @@ export default {
   },
 };
 </script>
+<style scoped>
+.btn {
+  border: 3px solid transparent;
+  border-radius: 3px;
+  padding: 5px;
+  cursor: pointer;
+  width: 100%;
+  display: block;
+}
+
+.btn--primary {
+  border-color: var(--primary-dark);
+  background-color: var(--primary);
+  color: #fff;
+}
+
+.btn--primary:hover {
+  border-color: var(--primary);
+  background-color: var(--primary-dark);
+}
+
+.btn--secondary {
+  border-color: var(--primary);
+  background-color: transparent;
+  color: var(--primary);
+}
+
+.btn--secondary:hover {
+  background-color: var(--primary);
+  color: #fff;
+}
+</style>
